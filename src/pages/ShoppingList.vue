ffc9ba3a5aeff73037f5086705558ef0099db27b
Simone Boscolo Berto <simone.boscoloberto@dapm.com>
2020-03-04 15:55:40 +0100

    Added confirm dialog and checkbox selection

--- src/pages/ShoppingList.vue
+++ src/pages/ShoppingList.vue
@@ -1,12 +1,22 @@
 <template>
   <q-page class="bg-grey-3 column">
-    <q-list v-for="item in list" :key="item.title" class="bg-white">
-      <q-item v-ripple>
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-for="(item, index) in list"
+        :key="index"
+        v-ripple
+        clickable
+        @click="item.done = !item.done"
+        :class="{ 'done bg-blue-1': item.done }"
+      >
         <q-item-section avatar>
-          <q-checkbox v-model="primary" color="primary" />
+          <q-checkbox class="no-pointer-events" v-model="item.done" color="primary" />
         </q-item-section>
         <q-item-section>
-          <q-item-label>{{item.title}}</q-item-label>
+          <q-item-label>{{ item.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="item.done" side>
+          <q-btn flat dense round color="primary" icon="delete" @click.stop="deleteItem(index)" />
         </q-item-section>
       </q-item>
     </q-list>
@@ -24,7 +34,7 @@ export default {
         },
         {
           title: 'Apples',
-          done: false
+          done: true
         },
         {
           title: 'Pears',
@@ -32,6 +42,30 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    deleteItem(index) {
+      this.$q
+        .dialog({
+          title: 'Confirm',
+          message: 'Do you really want to delete it?',
+          cancel: true,
+          persistent: true
+        })
+        .onOk(() => {
+          this.list.splice(index, 1)
+          this.$q.notify('Item deleted')
+        })
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
