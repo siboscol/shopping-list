5bb9c71ce363e476f3a596e5466184356b179522
Simone Boscolo Berto <simone.bb2003@gmail.com>
2020-10-04 19:26:01 +0200

    Reading firebase db data for current user

--- src/store/store-items.js
+++ src/store/store-items.js
@@ -2,27 +2,28 @@ import Vue from 'vue'
 import { uid } from 'quasar'
 import { storage } from 'firebase'
 import store from '.'
+import { firebaseDb, firebaseAuth } from 'boot/firebase'
 
 const state = {
   items: {
-    'id1': {
-      name: 'pippo',
-      done: false,
-      price: 0,
-      quantity: 1
-    },
-    'id2': {
-      name: 'pluto',
-      done: false,
-      price: 2,
-      quantity: 3
-    },
-    'id3': {
-      name: 'paperino',
-      done: false,
-      price: 0,
-      quantity: 1
-    },
+    // 'id1': {
+    //   name: 'pippo',
+    //   done: false,
+    //   price: 0,
+    //   quantity: 1
+    // },
+    // 'id2': {
+    //   name: 'pluto',
+    //   done: false,
+    //   price: 2,
+    //   quantity: 3
+    // },
+    // 'id3': {
+    //   name: 'paperino',
+    //   done: false,
+    //   price: 0,
+    //   quantity: 1
+    // },
   },
   search: ''
 }
@@ -59,6 +60,39 @@ const actions = {
   },
   setSearch({ commit }, value) {
     commit('setSearch', value)
+  },
+  fbReadData({ commit }, value) {
+    const userId = firebaseAuth.currentUser.uid
+    const userItems = firebaseDb.ref('items/' + userId)
+
+    // Child Added
+    userItems.on('child_added', snapshot => {
+      const item = snapshot.val()
+
+      const payload = {
+        id: snapshot.key,
+        item: item
+      }
+      commit('addItem', payload)
+    })
+
+    // Child Changed
+    userItems.on('child_changed', snapshot => {
+      const item = snapshot.val()
+
+      const payload = {
+        id: snapshot.key,
+        updates: item
+      }
+      commit('updateItem', payload)
+    })
+
+    
+    // Child Removed
+    userItems.on('child_removed', snapshot => {
+      const itemId = snapshot.key
+      commit('deleteItem', itemId)
+    })
   }
 }
 
