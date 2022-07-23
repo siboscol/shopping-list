265986e35231228b90dfef20adaa5f80427ad2bc
Simone Boscolo Berto <simone.bb2003@gmail.com>
2022-07-23 20:43:38 +0200

    First draft of scaper

---
+++ scaper.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const url = 'https://www.coop.ch/en/promotions/weekly-special-offers/special-offers-fruits-vegetables/c/m_1434';
+  const browser = await puppeteer.launch({ headless: false, slowMo: 250, devtools: true, args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  try {
+
+    const promotions = await page.evaluate(() => {
+      const products = document.querySelectorAll('div.productTile__wrapper');
+      const results = []
+      const textContent = elem => elem ? elem.textContent.trim() : '';
+
+      products.forEach(product => {
+        results.push(
+          {
+            title: textContent(product.querySelector('p.productTile-details__name-value')),
+            price: textContent(product.querySelector('p.productTile__price-value-lead-price')),
+            oldPrice: textContent(product.querySelector('p.productTile__price-value-lead-price-old')).replace('was  ', ''),
+            imageUrl: product.querySelector('img.product-listing__thumbnail__image').getAttribute('src')
+          }
+        )
+      })
+      return results;
+    })
+    console.log(promotions)
+    const promotionsString = JSON.stringify(promotions);
+    fs.writeFile("promotions.json", promotionsString, (err, result) => {
+      if(err) console.log('error', err);
+  });
+
+  } catch (error) {
+    console.log(error);
+  }
+  await browser.close();
+})();
